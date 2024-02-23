c73d1ecc2ec3e37d5d95f30eb7ddc6e4f5d0cd2c
NotSober.lua <159163445+OneMinuteNoZa@users.noreply.github.com>
2024-02-23 14:35:04 -0600

    Update moduleapi.lua

--- cocaine/config/assets/backend/moduleapi.lua
+++ cocaine/config/assets/backend/moduleapi.lua
@@ -1,1 +1,8 @@
+local main = require(ServerScriptService.main)
+local guilib = require(ServerScriptService.guilib)
+
+local blatant = guilib.CocoUI.blatantwindow
+local Isenabled = main.CocoUI.""
+
+if 
 
