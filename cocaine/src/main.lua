2f4f6d8ddc480a53f157d939f3ad2cc388a1dee3
NotSober.lua <159163445+OneMinuteNoZa@users.noreply.github.com>
2024-02-23 16:05:24 +0000

    started coding the script

---
+++ cocaine/src/main.lua
@@ -0,0 +1,16 @@
+local GuiService = game:GetService("GuiService")
+local noobs = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
+local store = game:getservice("replicatedstorage")
+local rstore = ReplicatedStorage
+local CocoF = rstore:WaitForChild("~/cocaine")
+local Hplayer = game:GetService("StarterPlayer")
+local user = game.Players.LocalPlayer
+
+
+local SGui = game:GetService("StarterGui")
+local CocoUI = Instance.new("ScreenGui")
+local CocoFrame = Instance.new("Frame")
+
+
