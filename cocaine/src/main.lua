1968fd3dbbe911560cd5a50ae6db66c56eb52bef
NotSober.lua <159163445+OneMinuteNoZa@users.noreply.github.com>
2024-03-12 12:37:30 -0500

    gonna use a prebuilt gui (im lazy :p)

--- cocaine/src/main.lua
+++ cocaine/src/main.lua
@@ -1,16 +1,3 @@
-local GuiService = game:GetService("GuiService")
-local noobs = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local StarterGui = game:GetService("StarterGui")
-local store = game:getservice("replicatedstorage")
-local rstore = ReplicatedStorage
-local CocoF = rstore:WaitForChild("~/cocaine")
-local Hplayer = game:GetService("StarterPlayer")
-local user = game.Players.LocalPlayer
-
-
-local SGui = game:GetService("StarterGui")
-local CocoUI = Instance.new("ScreenGui")
-local CocoFrame = Instance.new("Frame")
+gonna use a prebuilt ui cuz lazy
 
 
